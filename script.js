4b66f8f1c7dbed0f169a74d13cd547adaeb65526
pyeong0 <e1im7@naver.com>
2023-12-30 16:44:00 +0900

    re update

---
+++ script.js
@@ -0,0 +1,83 @@
+/* text_iife.js */
+// 텍스트 작성과 삭제 즉시 실행 함수
+(function () {
+  const spanEl = document.querySelector("main h2 span");
+  const txtArr = ["I am eunpyeong", "Thank you for visiting"];
+
+  // const txtArr = ['Web Publisher', 'Front-End Developer', 'Web UI Designer', 'UX Designer', 'Back-End Developer'];
+  let index = 0;
+  let currentTxt = txtArr[index].split("");
+  function writeTxt() {
+    spanEl.textContent += currentTxt.shift();
+    if (currentTxt.length !== 0) {
+      setTimeout(writeTxt, Math.floor(Math.random() * 100));
+    } else {
+      currentTxt = spanEl.textContent.split("");
+      setTimeout(deleteTxt, 3000);
+    }
+  }
+  function deleteTxt() {
+    currentTxt.pop();
+    spanEl.textContent = currentTxt.join("");
+    if (currentTxt.length !== 0) {
+      setTimeout(deleteTxt, Math.floor(Math.random() * 100));
+    } else {
+      index = (index + 1) % txtArr.length;
+      currentTxt = txtArr[index].split("");
+      writeTxt();
+    }
+  }
+  writeTxt();
+})();
+/* end text_iife.js */
+
+/* scroll_request.js */
+/* 수직 스크롤이 발생하면 header 태그에 active 클래스 추가 및 삭제 */
+const headerEl = document.querySelector("header");
+window.addEventListener("scroll", function () {
+  requestAnimationFrame(scrollCheck);
+});
+function scrollCheck() {
+  let browerScrollY = window.scrollY ? window.scrollY : window.pageYOffset;
+  if (browerScrollY > 0) {
+    headerEl.classList.add("active");
+  } else {
+    headerEl.classList.remove("active");
+  }
+}
+/* end scroll_request.js */
+
+/* move.js */
+/* 애니메이션 스크롤 이동 */
+const animationMove = function (selector) {
+  // ① selector 매개변로 이동할 대상 요소 노드 가져오기
+  const targetEl = document.querySelector(selector);
+  // ② 현재 브라우저의 스크롤 정보(y 값)
+  const browserScrollY = window.pageYOffset;
+  // ③ 이동할 대상의 위치(y 값)
+  const targetScorllY = targetEl.getBoundingClientRect().top + browserScrollY;
+  // ④ 스크롤 이동
+  window.scrollTo({ top: targetScorllY, behavior: "smooth" });
+};
+// 스크롤 이벤트 연결하기
+const scollMoveEl = document.querySelectorAll("[data-animation-scroll='true']");
+for (let i = 0; i < scollMoveEl.length; i++) {
+  scollMoveEl[i].addEventListener("click", function (e) {
+    const target = this.dataset.target;
+    animationMove(target);
+  });
+}
+/* End move.js */
+
+// document.querySelector('.mouse').addEventListener('click', function() {
+// location.reload();
+// });
+//
+
+// function rand(min, max) {
+// return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+// $(document).ready(function () {
+// var idx = rand(1, 5); // 랜덤으로 1에서 5까지 중 하나의 숫자를 선택
+// $(".eun-main-head__video.index" + idx).show();
+// });
